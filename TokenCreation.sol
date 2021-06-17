cf8d5253b627b674d81b4a22d631749805a11976
CatspersCoffee <40218126+CatspersCoffee@users.noreply.github.com>
2021-06-17 22:32:12 +1200

    Update TokenCreation.sol

--- TokenCreation.sol
+++ TokenCreation.sol
@@ -22,7 +22,8 @@ along with the DAO.  If not, see <http://www.gnu.org/licenses/>.
  * Token Creation parameters
 */
 
-import "./Token.sol";
+//import "./Token.sol";
+import "https://github.com/CatspersCoffee/DAO/blob/develop/Token.sol";
 
 pragma solidity ^0.4.4;
 
